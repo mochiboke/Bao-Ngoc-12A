c94489bb9c4568b50449e9b9ab0523ff82ac8ac2
mochiboke <nguyenbaongoccrlc@gmail.com>
2025-03-25 09:41:19 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login() {
+let a = document.getElementByld("username") .value;
+let b = document.getflementById("password") .value;
+if (a === "mochi"
+&&b === *mochicute") {
+window.open ("home.html");
+document.getElenentById("compiler").style.display = "block";
+} else {
+document.getelementById("error-message").innerText="Không hợp lệ “;}
+}
